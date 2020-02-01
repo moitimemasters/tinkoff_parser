cf5846866dc5dca448a06ac0611d544658f5e384
moitimemasters <ivanpesnia04@gmail.com>
2020-02-01 13:51:46 +0300

    data is structed

--- parse.py
+++ parse.py
@@ -5,3 +5,12 @@ data = requests.get(URL).json()
 
 contest_data, users_data = data["contests"], data["users"]
 users = {}
+# id : short, name, group
+for i in users_data:
+    users[i["id"]] = [i["group_short"], i["name"], i["group"]]
+
+contests = {}
+
+# id : title, date, problems, users
+for i in contest_data:
+    contests[i["id"]] = [i["title"], i["date"], i["problems"], i["users"]]
