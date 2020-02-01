0396becd91c363385af787e8385f9135e2b452ca
moitimemasters <ivanpesnia04@gmail.com>
2020-02-01 13:59:51 +0300

    get solved problems for user in each contest

--- parse.py
+++ parse.py
@@ -14,3 +14,16 @@ contests = {}
 # id : title, date, problems, users
 for i in contest_data:
     contests[i["id"]] = [i["title"], i["date"], i["problems"], i["users"]]
+
+
+# id, user_id -> returns list of problems, solved in given contest
+def parse_contest(id, user_id):
+    contest = contests[id][3]
+    user = contest[user_id]
+    result = []
+    problem_number = 0
+    for i in user:
+        if i["verdict"] == "OK":
+            result += [(problem_number, i["time"])]
+        problem_number += 1
+    return result
